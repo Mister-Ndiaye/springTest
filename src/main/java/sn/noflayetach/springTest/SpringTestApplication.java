6b9ecdd6fca54e5b546dc4a3c6979d15d1102e5f
Mister-Ndiaye <61950912+Mister-Ndiaye@users.noreply.github.com>
2023-04-04 13:28:27 +0000

    main

---
+++ src/main/java/sn/noflayetach/springTest/SpringTestApplication.java
@@ -0,0 +1,20 @@
+package sn.noflayetach.springTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringTestApplication {
+
+	@GetMapping
+	public String sayHello(){
+		return "Hello from Noflaye Tech ";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+}
